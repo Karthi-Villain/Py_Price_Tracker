2df7a738d14ae73b4fcd7ff8c53c01763cc2384e
Karthi <pongurkarthik1234@gmail.com>
2023-04-27 22:11:39 +0530

    update

--- pages/Scrape.py
+++ pages/Scrape.py
@@ -39,15 +39,11 @@ def Scraper_Fun(URL):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
     req=scraper.get(URL,headers=headers)
     bsoup = BeautifulSoup(req.text,"html.parser")
-    #print(req.content)
-    
     if req.status_code!=200:
         pass
     else:
         if("amazon" in req.url):
             Amazon_Link(bsoup)
-        
-
 
 with open(os.path.join(os.path.split(os.path.dirname(__file__))[0], 'Scraper_List.csv'),"r") as SL:
     Details=csv.reader(SL)
